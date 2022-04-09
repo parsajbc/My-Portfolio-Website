5a33aa3d7488b9ec2b158f10685a283cc2ad66f5
parsahj <parsa.jadda@mail.utoronto.ca>
2022-04-09 19:47:17 -0400

    added resume

--- index.js
+++ index.js
@@ -22,3 +22,28 @@ window.onclick = function (event) {
         navbar.style.display = "flex";
     }
 }
+
+resume = document.getElementById("resume_tab2");
+
+var resScroll = function () {
+    var y = window.scrollY;
+    if (y >= 250) {
+        resume.style.display = "block";
+        resume.setAtribute("onmouseover", "castShadow()");
+    } else {
+        resume.style.display = "none";
+        resume.setAtribute("onmouseover", "");
+    }
+};
+
+window.addEventListener("scroll", resScroll);
+
+function castShadow() {
+    var shadow = document.getElementById("shadow");
+    shadow.style.display = "block";
+}
+
+function uncastShadow() {
+    var shadow = document.getElementById("shadow");
+    shadow.style.display = "none";
+}
